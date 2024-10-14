e65450ceb12062696c187ff812b7fbf41d68b245
Raghav Kumar <raghavrk2675@gmail.com>
2024-10-15 02:48:37 +0530

    second commit

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <nav className='border-b border-white flex items-center justify-between h-16 pb-2 px-3'> {/* Added horizontal padding */}
+        <nav className='border-b border-white flex items-center justify-between h-20 pb-2 px-3'> {/* Added horizontal padding */}
             <div className='flex items-center'>
-                <span className='text-white text-2xl pt-2'>&#9776;</span> {/* Menu icon */}
+                <span className='text-white text-2xl pt-2 cursor-pointer'>&#9776;</span> {/* Menu icon */}
             </div>
             <div className='flex flex-col items-center flex-grow'> {/* Centering headings */}
-                <h1 className='text-white text-xl text-center pl-8'>Auto-Up-Keeper</h1>
-                <h1 className='text-white text-xs text-center pl-8'>A FIXATION TO VEXATIONS</h1>
+                <h1 className='text-white text-xl text-center pl-8 cursor-pointer pt-1'>Auto-Up-Keeper</h1>
+                <h1 className='text-white text-xs text-center pl-8 cursor-pointer'>A FIXATION TO VEXATIONS</h1>
             </div>
-            <h1 className='text-white text-sm pt-2'>TUTORIAL</h1> {/* Right heading */}
+            <h1 className='text-white text-sm pt-2 cursor-pointer'>TUTORIAL</h1> {/* Right heading */}
             
         </nav>
     );
--- app/Components/Main.js
+++ app/Components/Main.js
@@ -4,12 +4,15 @@ const Main = () => {
   return (
     <main className='flex items-center justify-center h-screen'> {/* Set height to fill the screen */}
       <div className='flex flex-col items-center'> {/* Stack items vertically */}
-        <h1 className='text-center text-4xl mb-4'>A website aiming to simplify car maintenance</h1> {/* Margin-bottom for spacing */}
+        <h1 className='text-center text-4xl mb-4 mt-32'>A website aiming to simplify car maintenance</h1> {/* Margin-bottom for spacing */}
         <button
-          className="bg-black text-white px-9 py-2 font-bold border border-white mt-40"> {/* Removed flex classes from button */}
+          className="bg-black text-white px-9 py-2 font-bold border border-white mt-24 mb-20"> {/* Removed flex classes from button */}
           Discover
         </button>
+        <br></br>
+        <h1 className='align-text-bottom text-xl mt-10 mb-20'> Navigate through menu bar</h1> {/* Margin-bottom for spacing */}
       </div>
+      
     </main>
   );
 };
